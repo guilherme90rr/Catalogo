b9db7ee8e9303bf2ddad465538074067402dc1a4
Guilherme Ferreira <101301922+guilherme90rr@users.noreply.github.com>
2023-04-01 22:55:29 -0300

    Base do crud
    Adicionado as classes utilizadas.

---
+++ Catalogos/Controllers/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+namespace Catalogos.Controllers.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? ImagemUrl { get; set; }
+    }
+}
