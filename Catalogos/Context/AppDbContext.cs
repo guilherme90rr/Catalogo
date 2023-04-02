28321be0c13ea16206eeb7d1f4cadff2c2f6d9e8
Guilherme Ferreira <101301922+guilherme90rr@users.noreply.github.com>
2023-04-02 00:45:21 -0300

    Conexão com banco de dados
    Configurado a conexão com banco de dados para conexão direta.

---
+++ Catalogos/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Catalogos.Controllers.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalogos.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options ) : base (options ) 
+        { }
+
+        public DbSet<Categoria>? Categorias { get; set; }
+        public DbSet<Produto>? Produtos { get; set; }
+
+
+    }
+}
--- Catalogos/Program.cs
+++ Catalogos/Program.cs
@@ -1,3 +1,6 @@
+using Catalogos.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string mySqlConnecton = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+                    options.UseMySql(mySqlConnecton,
+                    ServerVersion.AutoDetect(mySqlConnecton)));
 
 var app = builder.Build();
 
